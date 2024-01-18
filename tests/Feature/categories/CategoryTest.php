87b9839e9d9747599bcdc5fec2d90697d94d2f15
yazdan <yazdaner8@gmail.com>
2024-01-18 17:00:44 +0330

    categories 1.4.0 (test)

--- tests/Feature/categories/CategoryTest.php
+++ tests/Feature/categories/CategoryTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\categories;
 use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Support\Str;
+use Illuminate\Http\UploadedFile;
 use Modules\categories\App\Models\Category;
 
 class CategoryTest extends TestCase
@@ -17,83 +18,99 @@ class CategoryTest extends TestCase
             'name' => $category->name,
             'en_name' => $category->en_name,
             'icon' => $category->icon,
+            'image' => UploadedFile::fake()->image('pic.png')
         ]);
+        $latest = Category::latest('id')->first();
         //
+        $this->assertNotNull($latest->icon);
+        $response->assertOk();
+    }
+
+    public function test_show(): void
+    {
+        $category = Category::factory()->create([
+            'slug' => 'slugTest'
+        ]);
+        $response = $this->get('api/admin/categories/' . $category->id);
+        $body = json_decode($response->getContent());
+        //
+        $this->assertEquals($category->id, $body->id);
         $response->assertOk();
     }
 
     public function test_update(): void
     {
         $name = Str::random(10);
-        $link = fake()->url();
-        $phone_number = fake()->phoneNumber();
-        $category = Category::factory()->create();
+        $en_name = Str::random(10);
+        $category = Category::factory()->create([
+            'slug' => 'slugTest'
+        ]);
         $response = $this->put('api/admin/categories/' . $category->id, [
             'name' => $name,
-            'link' => $link,
-            'phone_number' => $phone_number,
+            'en_name' => $en_name,
         ]);
         //
         $this->assertDatabaseHas('categories', [
             'id' => $category->id,
             'name' => $name,
-            'link' => $link,
-            'phone_number' => $phone_number,
+            'en_name' => $en_name,
         ]);
         $response->assertOk();
     }
 
-    // public function test_show(): void
-    // {
-    //     $category = Category::factory()->create();
-    //     $response = $this->get('api/admin/categories/' . $category->id);
-    //     $body = json_decode($response->getContent());
-    //     //
-    //     $this->assertEquals($category->id, $body->id);
-    //     $response->assertOk();
-    // }
+    public function test_index(): void
+    {
+        $response = $this->get('api/admin/categories');
+        $body = json_decode($response->getContent(),true);
+        //
+        $this->assertArrayHasKey('categories',$body);
+        $response->assertOk();
+    }
 
-    // public function test_index(): void
-    // {
-    //     $response = $this->get('api/admin/categories');
-    //     $body = json_decode($response->getContent(),true);
-    //     //
-    //     $this->assertArrayHasKey('categories',$body);
-    //     $response->assertOk();
-    // }
+    public function test_index_search(): void
+    {
+        $response = $this->get('api/admin/categories?trashed=true&name=app');
+        $body = json_decode($response->getContent(),true);
+        $count = Category::onlyTrashed()->where('name','like','%app%')->count();
+        //
+        $this->assertEquals($body['categories']['total'],$count);
+        $this->assertArrayHasKey('categories',$body);
+        $response->assertOk();
+    }
 
-    // public function test_index_search(): void
-    // {
-    //     $response = $this->get('api/admin/categories?trashed=true&name=app');
-    //     $body = json_decode($response->getContent(),true);
-    //     $count = Category::onlyTrashed()->where('name','like','%app%')->count();
-    //     //
-    //     $this->assertEquals($body['categories']['total'],$count);
-    //     $this->assertArrayHasKey('categories',$body);
-    //     $response->assertOk();
-    // }
+    public function test_destroy(): void
+    {
+        $category = Category::factory()->create([
+            'slug' => 'slugTest'
+        ]);
+        $response = $this->delete('api/admin/categories/'.$category->id);
+        $this->assertDatabaseMissing('categories',[
+            'id' => $category->id,
+            'deleted_at' => null,
+        ]);
+        $response->assertOk();
+    }
 
-    // public function test_destroy(): void
-    // {
-    //     $category = Category::factory()->create();
-    //     $response = $this->delete('api/admin/categories/'.$category->id);
-    //     $this->assertDatabaseMissing('categories',[
-    //         'id' => $category->id,
-    //         'deleted_at' => null,
-    //     ]);
-    //     $response->assertOk();
-    // }
+    public function test_restore(): void
+    {
+        $category = Category::factory()->create([
+            'slug' => 'slugTest',
+            'deleted_at' => Carbon::now()
+        ]);
+        $response = $this->post('api/admin/categories/'.$category->id.'/restore');
+        $this->assertDatabaseHas('categories',[
+            'id' => $category->id,
+            'deleted_at' => null,
+        ]);
+        $response->assertOk();
+    }
 
-    // public function test_restore(): void
-    // {
-    //     $category = Category::factory()->create([
-    //         'deleted_at' => Carbon::now()
-    //     ]);
-    //     $response = $this->post('api/admin/categories/'.$category->id.'/restore');
-    //     $this->assertDatabaseHas('categories',[
-    //         'id' => $category->id,
-    //         'deleted_at' => null,
-    //     ]);
-    //     $response->assertOk();
-    // }
+    public function test_all(): void
+    {
+        $response = $this->get('api/categories/list');
+        $body = json_decode($response->getContent(),true);
+        $categories = Category::get();
+        $this->assertEquals(sizeof($categories),sizeof($body));
+        $response->assertOk();
+    }
 }
