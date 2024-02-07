e82f138b588ca5fb00cfb2dfff233a8c226a1c7f
yazdan <yazdaner8@gmail.com>
2024-02-07 17:52:54 +0330

    Variation Request

---
+++ modules/variations/App/Http/Requests/VariationRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class VariationRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        $rules = [
+            'price1' => ['required', 'numeric'],
+            'price2' => ['required', 'numeric'],
+            'product_count' => ['required', 'numeric'],
+        ];
+
+        if (!empty($this->request->get('preparation_time'))) {
+            $rules['preparation_time'] = ['numeric'];
+        }
+
+        return $rules;
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'price1' => 'قیمت محصول',
+            'price2' => 'قیمت محصول برای فروش',
+            'product_count' => 'تعداد موجودی محصول',
+            'max_product_cart' => 'حد اکثر تعداد سفارش در سبد خرید',
+            'preparation_time' => 'زمان آماده سازی محصول',
+        ];
+    }
+
+    protected function getValidatorInstance()
+    {
+        if ($this->request->has('price1')) {
+            $this->merge([
+                'price' => str_replace(',', '', $this->request->get('price1'))
+            ]);
+        }
+        return parent::getValidatorInstance();
+    }
+}
