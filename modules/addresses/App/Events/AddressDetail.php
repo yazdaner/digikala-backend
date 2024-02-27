a6c7e73f51ebdc6368c19980ec8f45db3522ccea
yazdan <yazdaner8@gmail.com>
2024-02-27 21:28:38 +0330

    address module 1.1.0

--- modules/addresses/App/Events/AddressDetail.php
+++ modules/addresses/App/Events/AddressDetail.php
@@ -2,10 +2,12 @@
 
 namespace Modules\addresses\App\Events;
 
+use Modules\addresses\App\Models\Address;
+
 class AddressDetail
 {
-    public function handle()
+    public function handle($where)
     {
-        
+        return Address::where($where)->first();
     }
 }
--- modules/addresses/App/Jobs/CreateStaticMap.php
+++ modules/addresses/App/Jobs/CreateStaticMap.php
@@ -7,24 +7,36 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Http;
+use Modules\addresses\App\Models\Address;
 
 class CreateStaticMap implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public function __construct()
+    protected int $addressId;
+
+    public function __construct($addressId)
     {
+        $this->addressId = $addressId;
     }
 
     public function handle(): void
     {
-    }
-
-    protected function sendEmail()
-    {
-    }
-
-    protected function sendSMS()
-    {
+        $address = Address::find($this->addressId);
+        if ($address) {
+            $url = 'https://map.ir/static?width=300&height=300&zoom_level=12&markers=color:red';
+            try {
+                $response = Http::withHeaders([
+                    'x-api-key' => env('MAP_IR_API_KEY'),
+                    'markers' => 'color:red|' . $address->longitude . ',' . $address->latitude
+                ])->get($url);
+                $path = 'upload/address-' . $address->id . '.png';
+                file_put_contents(fileDirectory($path), $response->body());
+                $address->staticmap = $path;
+                $address->update();
+            } catch (\Throwable $th) {
+            }
+        }
     }
 }
