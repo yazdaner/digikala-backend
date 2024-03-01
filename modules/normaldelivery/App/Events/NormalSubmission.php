fc4ede87bd6bab94ca31690cd558ad3a67c2f09d
yazdan <yazdaner8@gmail.com>
2024-03-01 20:56:37 +0330

    normal delivery module 1.5.1

--- modules/normaldelivery/App/Events/NormalSubmission.php
+++ modules/normaldelivery/App/Events/NormalSubmission.php
@@ -14,5 +14,43 @@ class NormalSubmission
 
     public function handle($data)
     {
+        $products = $data['products'];
+        $this->sender = $data['sender'];
+        $request = request();
+        if(defined('address_id')){
+            $address = runEvent('address-detail',[
+                'id' => address_id,
+                'user_id' => $request->user()->id,
+            ],true);
+            if($address){
+                $this->productsFiltering($products);
+                $intervals = $this->getTimeIntervals(
+                    $address->id,
+                    $this->sender
+                );
+                return [
+                    'selected_keys' => $this->selectedKeys,
+                    'products' => $this->filteredProducts,
+                    'intervals' => $intervals,
+                    'component' => 'normal-submission',
+                    'totalPrice' => $this->totalPrice,
+                    'finalPrice' => $this->finalPrice,
+                    'icon' => assert('upload/normal-delivery.png'),
+                    'name' => 'normal-delivery',
+                    'sender' => $this->sender,
+                ];
+            }
+        }
+    }
+
+    protected function productsFiltering($products)
+    {
+        foreach ($products as $key => $product) {
+            $variation = $product->variation;
+            if($product->product_dimensions == 'small' ||
+                $product->product_dimensions == 'medium'){
+                    
+                }
+        }
     }
 }
