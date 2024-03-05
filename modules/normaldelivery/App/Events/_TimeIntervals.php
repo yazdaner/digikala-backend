b0108995b1dd8f08321180cb4c6611c9deb1c5c9
yazdan <yazdaner8@gmail.com>
2024-03-05 20:43:24 +0330

    _TimeIntervals

--- modules/normaldelivery/App/Events/_TimeIntervals.php
+++ modules/normaldelivery/App/Events/_TimeIntervals.php
@@ -2,6 +2,9 @@
 
 namespace Modules\normaldelivery\App\Events;
 
+use Modules\core\Lib\Jdf;
+use Modules\normaldelivery\App\Models\IntervalsNormalPosting;
+
 class _TimeIntervals
 {
     protected int $delivery_period;
@@ -9,7 +12,9 @@ class _TimeIntervals
 
     protected function getTimeIntervals($cityId, $sender)
     {
-        $this->setDeliveryPeriod();
+        $this->setDeliveryPeriod($cityId, $sender);
+        $this->delivery_period_timestamp = strtotime("+ {$this->delivery_period} days");
+        $days = $this->recordedDay($cityId, $sender);
     }
 
     protected function setDeliveryPeriod($cityId, $sender)
@@ -32,7 +37,19 @@ class _TimeIntervals
         }
 
         if(intval($normal_delivery_time) > 0){
-
+            $period += intval($normal_delivery_time);
         }
+        $this->delivery_period = $period;
+    }
+    protected function recordedDay($cityId, $sender){
+        $jdf = new Jdf;
+        $firstDay = $jdf->tr_num(
+            $jdf->jdate('Y/n/j',$this->delivery_period_timestamp)
+        );
+        $row = IntervalsNormalPosting::where([
+            'city_id' => $cityId,
+            'day' => $firstDay,
+            'sender' => $sender
+        ])->first();
     }
 }
