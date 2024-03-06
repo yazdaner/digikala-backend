1230d70ed5324476b6f8f9d448a90c193750cda4
yazdan <yazdaner8@gmail.com>
2024-03-06 23:03:09 +0330

    _TimeIntervals 1.1.0

--- modules/normaldelivery/App/Events/_TimeIntervals.php
+++ modules/normaldelivery/App/Events/_TimeIntervals.php
@@ -15,6 +15,11 @@ class _TimeIntervals
         $this->setDeliveryPeriod($cityId, $sender);
         $this->delivery_period_timestamp = strtotime("+ {$this->delivery_period} days");
         $days = $this->recordedDay($cityId, $sender);
+        if (sizeof($days) > 0) {
+            return $this->createIntervalsFromDefindDays($days);
+        } else {
+            return $this->createDaynamicIntervals($cityId, $sender);
+        }
     }
 
     protected function setDeliveryPeriod($cityId, $sender)
@@ -36,20 +41,62 @@ class _TimeIntervals
             );
         }
 
-        if(intval($normal_delivery_time) > 0){
+        if (intval($normal_delivery_time) > 0) {
             $period += intval($normal_delivery_time);
         }
         $this->delivery_period = $period;
     }
-    protected function recordedDay($cityId, $sender){
+
+    protected function recordedDay($cityId, $sender)
+    {
         $jdf = new Jdf;
-        $firstDay = $jdf->tr_num(
-            $jdf->jdate('Y/n/j',$this->delivery_period_timestamp)
-        );
-        $row = IntervalsNormalPosting::where([
+        $days = [];
+        for ($i = $this->delivery_period; $i < ($this->delivery_period + 7); $i++) {
+            $timestamp = strtotime("+ {$i} days");
+            $days[] = $jdf->jdate('Y/n/j', $timestamp);
+        }
+        return IntervalsNormalPosting::whereIn('date', $days)
+            ->where([
+                'city_id' => $cityId,
+                'sender' => $sender,
+            ])
+            ->orderBy('id', 'ASC')
+            ->get()
+            ->unique('date')
+            ->take(5);
+    }
+
+    protected function createDaynamicIntervals($cityId, $sender)
+    {
+        $daynamic = IntervalsNormalPosting::where([
+            'date' => '*',
             'city_id' => $cityId,
-            'day' => $firstDay,
-            'sender' => $sender
+            'sender' => $sender,
         ])->first();
+        $list = [];
+        $weekOfDayArr = [];
+        if ($daynamic) {
+            $jdf = new Jdf;
+            $period = $this->delivery_period;
+            $dateArr = [];
+            for ($i = $period; $i <= ($period + 10); $i++) {
+                $timestamp = strtotime("+ {$i} days");
+                $l = $jdf->jdate('l', $timestamp);
+                if ($l != 'جمعه') {
+                    $date = $jdf->jdate('Y/n/j', $timestamp);
+                    $dateArr[] = $date;
+                    $weekOfDayArr[$date] = $l;
+                }
+            }
+            $holidays = runEvent('get-holidays',$this->delivery_period_timestamp,true);
+            if(is_array($holidays)){
+                $dateArr = array_diff($dateArr,$holidays);
+            }
+        }
+        return $list;
+    }
+
+    protected function createIntervalsFromDefindDays($days)
+    {
     }
 }
