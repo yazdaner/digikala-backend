fd15073357ffc8a771df53c2d270ab51bc8d15f3
yazdan <yazdaner8@gmail.com>
2024-07-14 00:23:23 +0330

    seller 1.2.1

---
+++ modules/sellers/App/Http/Controllers/Auth/CheckVerificationContoller.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Modules\sellers\App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Modules\users\App\Models\Seller;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Contracts\Auth\PasswordBroker;
+use Modules\core\App\Models\VerificationCode;
+
+class CheckVerificationContoller extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $username = $request->get('username');
+        $code = $request->get('code');
+        $type = $request->get('type');
+        $seller = Seller::where('username',$username)->first();
+        if($seller){
+            $verification = VerificationCode::where([
+                'tableable_type' => Seller::class,
+                'tableable_id' => $seller->id,
+                'code' => $code
+            ])->first();
+
+            if ($verification) {
+
+                $verification->delete();
+                if ($type == 'forget-password') {
+                    $token = $this->broker()->sendResetLink(
+                        $request->only('username')
+                    );
+                    return ['status' => 'ok', 'token' => $token];
+                } else {
+                    $seller->status = -2;
+                    if (filter_var($seller->username, FILTER_VALIDATE_EMAIL)) {
+                        $seller->email = $seller->username;
+                    }else{
+                        $seller->mobile = $seller->username;
+                    }
+                    $seller->update();
+                    return ['status' => 'ok'];
+                }
+
+            }else{
+                return [
+                    'status' => 'error',
+                    'message' => 'کد تایید وارد شده اشتباه می باشد',
+                ];
+            }
+
+
+
+        }else{
+            return [
+                'status' => 'error',
+                'message' => 'فروشنده یافت نشد',
+            ];
+        }
+    }
+
+    protected function broker () : PasswordBroker
+    {
+        return Password::broker(
+            config('fortify.passwords')
+        );
+    }
+}
--- modules/sellers/routes/guest.php
+++ modules/sellers/routes/guest.php
@@ -7,13 +7,13 @@ use Modules\sellers\App\Http\Controllers\Auth\RegisterContoller;
 use Modules\sellers\App\Http\Controllers\Auth\AuthenticationContoller;
 use Modules\sellers\App\Http\Controllers\Auth\SellerPasswordContoller;
 use Modules\sellers\App\Http\Controllers\Auth\SendVerificationCodeContoller;
-use Modules\sellers\App\Http\Controllers\Auth\CheckVerificationCodeContoller;
+use Modules\sellers\App\Http\Controllers\Auth\CheckVerificationContoller;
 
 Route::prefix('seller')->middleware('guest')->group(function(){
     Route::post('sign-in',[AuthenticationContoller::class,'signIn']);
     Route::post('login',[AuthenticationContoller::class,'login']);
     Route::post('send/verify-code',SendVerificationCodeContoller::class);
-    Route::post('active-code/verify',CheckVerificationCodeContoller::class);
+    Route::post('active-code/verify',CheckVerificationContoller::class);
     Route::post('account/set-password',SellerPasswordContoller::class);
     Route::post('account/add-info',[RegisterContoller::class,'addInfo']);
     Route::post('account/final-step',[RegisterContoller::class,'finalStep']);
