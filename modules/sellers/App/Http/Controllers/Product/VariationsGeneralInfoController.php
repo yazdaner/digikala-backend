1ec498145d11e752283d82505bf4f21f87d2e244
yazdan <yazdaner8@gmail.com>
2024-07-18 21:45:07 +0330

    seller panel 1.0.1

--- modules/sellers/App/Http/Controllers/Product/VariationsGeneralInfoController.php
+++ modules/sellers/App/Http/Controllers/Product/VariationsGeneralInfoController.php
@@ -2,11 +2,71 @@
 
 namespace Modules\sellers\App\Http\Controllers\Product;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Modules\sellers\App\Models\SellerProduct;
 
 class VariationsGeneralInfoController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
+        $seller = $request->user();
+        $buyBoxVariationsCount = runEvent('variation:query', function ($query) use ($seller) {
+            return $query
+                ->where([
+                    'seller_id' => $seller->id,
+                    'selected_buy_box' => true
+                ])->count();
+        }, true);
+
+        $variationsCount = runEvent('variation:query', function ($query) use ($seller) {
+            return $query->where('seller_id', $seller->id)->count();
+        }, true);
+
+        $productsId = SellerProduct::where('seller_id', $seller->id)
+            ->pluck('product_id')
+            ->toArray();
+
+        $hasCompetitor = runEvent('variation:query', function ($query) use ($seller, $productsId) {
+            return $query->distinct('product_id')
+                ->where('seller_id', '!=', $seller->id)
+                ->whereIn('product_id', $productsId)
+                ->count();
+        }, true);
+
+        $uniqueVariationsCount = runEvent('variation:query', function ($query) use ($seller) {
+            return $query->distinct('product_id')
+                ->where('seller_id', $seller->id)
+                ->count();
+        }, true);
+
+        return [
+            [
+                'title' => 'تعداد کل تنوع ها',
+                'value' => $variationsCount
+            ],
+            [
+                'title' => 'تنوع های برنده شده باکس خرید',
+                'value' => $buyBoxVariationsCount
+            ],
+            [
+                'title' => 'تنوع های برنده نشده باکس خرید',
+                'value' => ($variationsCount - $buyBoxVariationsCount)
+            ],
+            [
+                'title' => 'تنوع های دارای رقیب',
+                'value' => $hasCompetitor,
+                'detail' => [
+                    'حداقل یک فروشنده روی کالای مشابه قیمت گذاری کرده است'
+                ]
+            ],
+            [
+                'title' => 'تنوع های بدون رقیب',
+                'value' => ($uniqueVariationsCount - $hasCompetitor),
+                'detail' => [
+                    'تنوع های که در حال حاضر شما تنها فروشنده آن هستید'
+                ]
+            ],
+        ];
     }
 }
--- modules/sellers/App/Providers/ModuleServiceProvider.php
+++ modules/sellers/App/Providers/ModuleServiceProvider.php
@@ -1,28 +1,47 @@
 <?php
+
 namespace Modules\sellers\App\Providers;
 
+use Modules\sellers\App\Models\Seller;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Builder;
 use Modules\sellers\App\Events\SellerAccess;
+use Modules\sellers\App\Models\SellerProduct;
 use Modules\sellers\App\Events\UpdateSellerProducts;
 use Modules\sellers\App\Http\Middleware\SellerAuthenticate;
 
 class ModuleServiceProvider extends ServiceProvider
 {
 
-    public function register() :void
+    public function register(): void
     {
         require_once base_path('modules/sellers/helpers.php');
         $this->loadMigrationsFrom(base_path('modules/sellers/database/migrations'));
-        $this->app['router']->aliasMiddleware('auth.seller',SellerAuthenticate::class);
-        addEvent('access:admin-check',SellerAccess::class);
+        $this->app['router']->aliasMiddleware('auth.seller', SellerAuthenticate::class);
+        addEvent('access:admin-check', SellerAccess::class);
         addEvent('product:created', UpdateSellerProducts::class);
-
     }
 
-    public function boot() :void
+    public function boot(): void
     {
         updateAuthConfigs();
+
+        Builder::macro('seller', function () {
+            return $this->getModel()->belongsTo(
+                Seller::class,
+                'seller_id',
+                'id'
+            )->withDefault([
+                'brandName' => config('shop-info.name')
+            ])->select(['id', 'brandName', 'logo']);
+        });
+
+        Builder::macro('product_seller', function () {
+            return $this->getModel()->belongsTo(
+                SellerProduct::class,
+                'id',
+                'seller_id'
+            );
+        });
     }
-
 }
-
