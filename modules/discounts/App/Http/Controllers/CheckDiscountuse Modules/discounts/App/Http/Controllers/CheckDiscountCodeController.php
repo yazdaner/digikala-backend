78a53e8fa6e7fd30d5558e638eda867ac4ba9245
yazdan <yazdaner8@gmail.com>
2025-02-05 23:41:43 +0330

    discount 1.2.1

--- modules/discounts/App/Http/Controllers/CheckDiscountuse Modules/discounts/App/Http/Controllers/CheckDiscountCodeController.php
+++ modules/discounts/App/Http/Controllers/CheckDiscountuse Modules/discounts/App/Http/Controllers/CheckDiscountCodeController.php
@@ -10,6 +10,7 @@ class CheckDiscountCodeController extends Controller
 {
     protected $products = [];
     protected $variations = [];
+    protected $grouped = [];
 
     public function __invoke(Request $request)
     {
@@ -19,18 +20,49 @@ class CheckDiscountCodeController extends Controller
         ]);
         $discounts = Discount::where('code', $request->post('code'))
             ->where('expiration_date', '>=', time())
-            ->orderBy('amount', 'DESC')
+            ->orderBy('min_purchase', 'DESC')
             ->get();
         if (sizeof($discounts)) {
             $cartItems = runEvent('cart:items', 1, true);
             if (sizeof($cartItems) > 0) {
                 $this->variations = $this->getVariations($cartItems);
                 $this->products = $this->getProducts($this->variations);
-                $grouped = [];
                 foreach ($discounts as $discount) {
-                    $grouped[$discount->category_id] =
-                        $this->getVariationsIdBasedCategory($discount->category_id);
+                    $variationsId = $this->getVariationsIdBasedCategory(
+                        $discount->category_id
+                    );
+                    $this->grouped[] =
+                        [
+                            'category_id' => $discount->category_id,
+                            'variationsId' => $variationsId,
+                            'totalPrice' => $this->getVariationsTotalPrice(
+                                $variationsId
+                            ),
+                            'discount' => $discount
+                        ];
                 }
+                $discountAmount = 0;
+                $i = 0;
+                while (sizeof($this->grouped) > 0) {
+                    $amount = $this->getGruopDiscountAmount(
+                        $this->grouped[$i]
+                    );
+                    if ($this->grouped[$i]['discount']->min_perchase > 0) {
+                        if (
+                            intval($this->grouped[$i]['totalPrice']) <
+                            intval($this->grouped[$i]['discount']->min_perchase)
+                        ) {
+                            $amount = 0;
+                        }
+                    }
+                    if ($amount > 0) {
+                        $discountAmount += $amount;
+                    }
+                    unset($this->grouped[$i]);
+                }
+
+
+                return ['discount' => $discountAmount];
             }
         }
         return $result;
@@ -57,5 +89,33 @@ class CheckDiscountCodeController extends Controller
                 ->pluck(['id', 'category_id'])
                 ->toArray();
         }, true);
+    }
+
+    protected function getVariationsIdBasedCategory($category_id)
+    {
+        if ($category_id == 0) {
+            return array_keys($this->variations);
+        } else {
+            $productsId = array_keys(array_filter($this->products, function ($value) use ($category_id) {
+                return $value == $category_id;
+            }));
+            return array_filter($this->variations, function ($variation) use ($productsId) {
+                return array_key_exists($variation->product_id, $productsId);
+            });
+        }
+    }
+
+    protected function getGruopDiscountAmount($data)
+    {
+        $amount = 0;
+        if (intval($data['discount']->amount) > 0) {
+            $amount = intval($data['discount']->amount);
+        } else {
+            $amount = ($data['totalPrice'] * intval($data['discount']->percent) / 100);
+        }
+        if (intval($data['discount']['max_amount']) > $amount) {
+            $amount = $data['discount']['max_amount'];
+        }
+        return intval($amount);
     }
 }
